2a04d9c22b1c6855e6362435f8d2c9fe841ef0af
Miyuru Dharmage <voxsar@gmail.com>
2022-12-18 11:26:44 +0530

    changes and base init  Changes to be committed: 	new file:   .github/workflows/master.yml 	new file:   LICENSE 	new file:   README.MD 	new file:   artslab-threejs-orbiter.php 	new file:   updater.php 	new file:   updates/elementor.svg 	new file:   updates/update-template.json 	new file:   updates/update.json

---
+++ artslab-threejs-orbiter.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: ThreeJs Oribiter for Elementor by Artslab Creatives
+ * Description: Adds a threejs scene in a component with the ability to add 3d models in a orbiter
+ * Plugin URI:  https://plugins.artslabcreatives.com
+ * Version:     1.0.0
+ * Author:      Artslab Creatives
+ * Author URI:  https://plugins.artslabcreatives.com
+ * Text Domain: artslab-threejs-orbiter
+ *
+ * Elementor tested up to: 3.7.0
+ * Elementor Pro tested up to: 3.7.0
+ */
+
+
+require_once( __DIR__ . '/updater.php' );
+new ALCTJSOpdater();
